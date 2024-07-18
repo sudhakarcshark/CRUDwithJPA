92df0d696cf4a775056151523497deac4d46823e
sudhakar <sudhakarvisam26@gmail.com>
2024-07-18 11:01:54 +0530

    Updated validation to Customer Fields

--- src/main/java/com/sb/curdwithjpa/controller/CustomerController.java
+++ src/main/java/com/sb/curdwithjpa/controller/CustomerController.java
@@ -5,8 +5,11 @@ import com.sb.curdwithjpa.request.CustomerRequest;
 import com.sb.curdwithjpa.request.EmailRequest;
 import com.sb.curdwithjpa.response.APIResponse;
 import com.sb.curdwithjpa.service.CustomerService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,8 +20,16 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @PostMapping("/create")
-    public ResponseEntity<APIResponse> createCustomer(@RequestBody CustomerRequest request){
-
+    public ResponseEntity<APIResponse> createCustomer(@RequestBody @Valid CustomerRequest request, BindingResult bindingResult){
+        if (bindingResult.hasErrors()){
+            return ResponseEntity.ok(
+                    APIResponse.builder()
+                            .errorCode(HttpStatus.BAD_REQUEST.value())
+                            .errorMessage(HttpStatus.BAD_REQUEST.toString())
+                            .data(bindingResult.getFieldErrors().stream().map(fieldError -> fieldError.getDefaultMessage()).toList())
+                            .build()
+            );
+        }
         return customerService.createCustomer(request);
 
     }
--- src/main/java/com/sb/curdwithjpa/request/CustomerRequest.java
+++ src/main/java/com/sb/curdwithjpa/request/CustomerRequest.java
@@ -1,6 +1,9 @@
 package com.sb.curdwithjpa.request;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.*;
 
 @Getter
@@ -11,9 +14,19 @@ import lombok.*;
 @Builder(toBuilder = true)
 public class CustomerRequest {
 
+    @NotBlank(message = "Please Provide Username")
     private String customerName;
+
     private int customerAge;
+
+    @NotBlank(message = "Mobile Number Should Not Be Empty")
+    @Size(min = 10,max = 10,message = "Please Input Valid Mobile Number")
     private String customerMobileNumber;
+
+    @NotBlank(message = "Email_Id Should not be Empty")
+    @Email(message = "Please Provide Valid Email_Id")
     private String customerEmailAddress;
+
+    @NotBlank(message = "Please Provide Address")
     private String customerAddress;
 }
